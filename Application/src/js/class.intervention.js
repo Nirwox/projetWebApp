cabe7c7802e229d680eca1c2053c9f83a9bbf2db
Nirwox <hugo.louvet@hotmail.fr>
2020-03-30 21:41:35 +0200

    Modification du tableau en dynamique

---
+++ Application/src/js/class.intervention.js
@@ -0,0 +1,12 @@
+class Intervention {
+    constructor(id ,userId ,nom, prenom, titre, description, date, status) {
+        this.id = id;
+        this.userId = userId;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.titre = titre;
+        this.description = description;
+        this.date = date;
+        this.status = status;
+    }
+}
--- Application/src/js/common.js
+++ Application/src/js/common.js
@@ -1,3 +1,37 @@
 $(document).ready( function () {
+    var lesInterv = [];
+    
+    var row = new Intervention(1, 'U00I701', 'Louvet', 'Hugo', 'Comment héberger plusieurs sites web sur un même hébergement mutualisé', 'Comment héberger plusieurs sites web sur un même hébergement mutualisé', '30/03/2020','Assigné');
+    var row2 = new Intervention(2, 'U00I908', 'Louvet', 'Hugo', 'Comment créer un site web', 'Comment créer un site web', '30/03/2020','Terminé');
+    var row3 = new Intervention(3, 'U00I876', 'Louvet', 'Hugo', "J'ai publié mon site mais la page « Félicitations » d'OVH s’affiche toujours", "J'ai publié mon site mais la page « Félicitations » d'OVH s’affiche toujours", '30/03/2020','Terminé');
+
+    lesInterv.push(row);
+    lesInterv.push(row2);
+    lesInterv.push(row3);
+
+    var lesLignes = document.getElementById('lesLignes');
+    lesInterv.forEach(oneIntervention => {
+        var newLigne = document.createElement('tr');
+
+        var newCellNom = document.createElement('td');
+        newCellNom.innerHTML = oneIntervention.id;
+    
+        var newCellPrenom = document.createElement('td');
+        newCellPrenom.innerHTML = oneIntervention.userId;
+    
+        var newCellTitre = document.createElement('td');
+        newCellTitre.innerHTML = oneIntervention.titre;
+    
+        var newCellDate = document.createElement('td');
+        newCellDate.innerHTML = oneIntervention.status;
+    
+        newLigne.appendChild(newCellNom);
+        newLigne.appendChild(newCellPrenom);
+        newLigne.appendChild(newCellTitre);
+        newLigne.appendChild(newCellDate);
+    
+        lesLignes.appendChild(newLigne)
+    });
+
     $('#myTable').DataTable();
 });
